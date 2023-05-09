5a70bf5cd58f0f66c61d7123996e755061fafdd6
Marta <marta.trebicka@wp.pl>
2023-05-09 18:30:57 +0200

    Add some final changes

---
+++ FrontOrangeJavaPL2/front-end-OrangeJavaPL2/src/app/Bread.ts
@@ -0,0 +1,8 @@
+export interface Bread {
+
+  id: number;
+  name: string;
+  breadType: string;
+  price: number;
+
+}
--- OrangeJavaPL2/src/main/java/pl/sda/OrangeJavaPL2/security/BakerySecurityConfig.java
+++ OrangeJavaPL2/src/main/java/pl/sda/OrangeJavaPL2/security/BakerySecurityConfig.java
@@ -6,29 +6,42 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class BakerySecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class BakerySecurityConfig  implements WebMvcConfigurer {
 
-       http
-               .csrf().disable()
-               .headers().disable()
-               .authorizeRequests()
-               .mvcMatchers("/*/**")
-               .permitAll();
+    //
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http
+//                .csrf().disable()
+//                .headers().disable()
+//                .authorizeRequests()
+//                .mvcMatchers("/*/**")
+//                .permitAll();
+//    }
+
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("adimin")
+//                .password("admin")
+//                .roles("ADMIN")
+//                .build();
+//        auth.inMemoryAuthentication()
+//                .withUser(user);
+//    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**");
     }
 
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("adimin")
-                .password("admin")
-                .roles("ADMIN")
-                .build();
-        auth.inMemoryAuthentication()
-                .withUser(user);
-    }
 }
