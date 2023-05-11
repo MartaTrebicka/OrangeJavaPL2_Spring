dfdd897e5437ff166804ca6fd69f6b42e38ea8ca
Marta <marta.trebicka@wp.pl>
2023-05-11 20:28:36 +0200

    @BeforeEach adnotation

--- TestingAdvanced/src/test/java/sda/testingadvanced/tdd/CalculatorTest.java
+++ TestingAdvanced/src/test/java/sda/testingadvanced/tdd/CalculatorTest.java
@@ -1,40 +1,50 @@
 package sda.testingadvanced.tdd;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class CalculatorTest {
 
     // KLASA TESTOWA JEST TYPU VOID !
 
+    //  @BeforeAll - run one time before first test
+    //  @AfterAll - run one time after last test
+    //  @AfterEach - run after each test
+    //  @BeforeEach - run before each test
+
+    Calculator calculator;
+    @BeforeEach
+    void createCalculator(){
+        calculator = new Calculator();
+
+    }
     @Test
-    void whenAddTwoIntegersThenReturnsSumOfTheseIntegers(){  // metoda
+    void whenAddTwoIntegersThenReturnsSumOfTheseIntegers() {  // metoda
 
         //given
-        Calculator calculator = new Calculator();
+       // Calculator calculator = new Calculator();  // tego nie trzeba już bo w @BeforeEach daliśmy calculator
         //when
-        int result = calculator.addTwoIntegers(2,3);
+        int result = calculator.addTwoIntegers(2, 3);
         //then
-        Assertions.assertEquals(5, result );
+        Assertions.assertEquals(5, result);
     }
 
     @Test
-    void whenAnyComponentIsGreaterThan100ThenIllegalExceptionIsThrown(){  // metoda
+    void whenAnyComponentIsGreaterThan100ThenIllegalExceptionIsThrown() {  // metoda
 
         //given
-        Calculator calculator = new Calculator();
+       // Calculator calculator = new Calculator(); // tego nie trzeba już bo w @BeforeEach daliśmy calculator
 
         //then
-      Assertions.assertThrows(IllegalArgumentException.class,
-              () -> calculator.addTwoIntegers(101, 2));
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> calculator.addTwoIntegers(101, 2));
     }
 
 
     @Test
-    void whenComponentBIsGreaterThanAThenIllegalExceptionIsThrown(){  // metoda
+    void whenComponentBIsGreaterThanAThenIllegalExceptionIsThrown() {  // metoda
 
         //given
-        Calculator calculator = new Calculator();
+       // Calculator calculator = new Calculator(); // tego nie trzeba już bo w @BeforeEach daliśmy calculator
 
         //then
         Assertions.assertThrows(IllegalArgumentException.class,
